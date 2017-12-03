778072c2395914081248278c717cb2d967bc318a
Amar Ahmed <amar11432@gmail.com>
2017-12-03 16:36:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/fragmentsapp/HelloFragment.java
@@ -0,0 +1,20 @@
+package com.example.android.fragmentsapp;
+
+/**
+ * Created by C4Q on 12/3/17.
+ */
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HelloFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /** Inflating the layout for this fragment **/
+        View v = inflater.inflate(R.layout.fragment_layout, null);
+        return v;
+    }
+}
--- app/src/main/java/com/example/android/fragmentsapp/MainActivity.java
+++ app/src/main/java/com/example/android/fragmentsapp/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.android.fragmentsapp;
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("");
+
+        Button btnLoad = (Button) findViewById(R.id.btn_load);
+
+        OnClickListener listener = new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                HelloFragment hello = new HelloFragment();
+                fragmentTransaction.add(R.id.fragment_container, hello, "HELLO");
+                fragmentTransaction.commit();
+            }
+        };
+
+        btnLoad.setOnClickListener(listener);
+
     }
 }
